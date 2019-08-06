4050af661c360269a5cf459b89a12e33345f6b6a
Vinnys Lins <vinnysfl@hotmail.com>
2019-08-06 00:38:29 -0300

    Added data_type module

---
+++ data_type/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	var (
+		a string
+		b int8
+		c uint8 // inteiro positivo
+		d int16
+		e uint16
+		f int32
+		g uint32
+		h int64
+		i uint64
+		j uint
+		l uintptr
+		m float32
+		n float64
+		o complex64 // números complexos
+		p complex128
+		q rune // referência pra um int32
+		r byte // referencia pra um uint8
+		x bool
+	)
+}
